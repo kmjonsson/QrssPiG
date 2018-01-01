09ceaac2c952860a9374a81b5ee7ec89c31430bd
Martin <sputnik@on-the-web.ch>
2018-01-01 13:53:57 +0100

    Update hackrf install script to install version 2017.02.01

--- scripts/install-libhackrf.sh
+++ scripts/install-libhackrf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-wget -O hackrf-v2015.07.2.tar.gz https://github.com/mossmann/hackrf/archive/v2015.07.2.tar.gz
-tar xf hackrf-v2015.07.2.tar.gz
+wget -O hackrf-v2017.02.1.tar.gz https://github.com/mossmann/hackrf/archive/v2017.02.1.tar.gz
+tar xf hackrf-v2017.02.1.tar.gz
 
-mkdir -p hackrf-2015.07.2/host/build && cd hackrf-2015.07.2/host/build && cmake ../ && make -j 4 && sudo make install && sudo ldconfig
+mkdir -p hackrf-2017.02.1/host/build && cd hackrf-2017.02.1/host/build && cmake ../ && make -j 4 && sudo make install && sudo ldconfig
